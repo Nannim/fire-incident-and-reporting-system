7b15c12a53f0bdf63af77aa2736155d088a0fd7f
Nannim David Dandam <dandamnannim125@gmail.com>
2022-02-22 09:33:06 +0100

    t
    d

--- resources/views/auto-complete-city.blade.php
+++ resources/views/auto-complete-city.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>User list - PDF</title>
+	<title>Address - Fire Incident and Reporting System</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maps.google.com/maps/api/js?key=AIzaSyBDJeyr-TVSrWkLte7JXXNrEDF9PQ1Qn3I&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
@@ -11,7 +11,7 @@
 <div class="container">
 	<form>
 		<div class="form-group">
-            <label>City<star>*</star></label>
+            <label>City</label>
             <input type="text" name="city" placeholder="City" class="form-control" value="{{ old('city') }}" id="city">
         </div>
 	</form>
